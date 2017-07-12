91e22de8d6880f8f12b7e5a9e03571de322145e8
Jay Vercellone <jay.vercellone@toptal.com>
2017-07-12 10:29:04 -0300

    Add code for the first plot

---
+++ plot1.R
@@ -0,0 +1,15 @@
+if (require(sqldf)) {
+    data <- read.csv2.sql(
+        "household_power_consumption.txt",
+        sql="SELECT * FROM file WHERE Date IN ('1/2/2007', '2/2/2007')")
+} else {
+    data <- read.csv2("household_power_consumption.txt")
+}
+
+png(file="plot1.png")
+hist(
+    data$Global_active_power,
+    main="Global Active Power",
+    xlab="Global Active Power (kilowatts)", 
+    col="red")
+dev.off()
