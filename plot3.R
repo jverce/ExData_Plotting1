43478ea83cc6953d200c925b7e33fed67163eb26
Jay Vercellone <jay.vercellone@toptal.com>
2017-07-16 21:07:22 -0300

    Fix legends

--- plot3.R
+++ plot3.R
@@ -37,7 +37,8 @@ with(
 with(data, lines(Date.Time, Sub_metering_2, col="red"))
 with(data, lines(Date.Time, Sub_metering_3, col="blue"))
 legend(
-    "topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+    "topright",
+    legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
     col=c("black", "red", "blue"),
     lty=c("solid", "solid", "solid"))
 
--- plot4.R
+++ plot4.R
@@ -51,6 +51,11 @@ with(
         xlab="", ylab="Energy sub metering"))
 with(data, lines(Date.Time, Sub_metering_2, col="red"))
 with(data, lines(Date.Time, Sub_metering_3, col="blue"))
+legend(
+    "topright", bty="n",
+    legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+    col=c("black", "red", "blue"),
+    lty=c("solid", "solid", "solid"))
 
 with(
     data, plot(
