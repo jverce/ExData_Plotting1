28836bb6e34cbd720b96c8bb508b9e37514a40eb
Jay Vercellone <jay.vercellone@toptal.com>
2017-07-16 21:04:29 -0300

    Add legend to the third plot

--- plot3.R
+++ plot3.R
@@ -36,5 +36,9 @@ with(
         xlab="", ylab="Energy sub metering"))
 with(data, lines(Date.Time, Sub_metering_2, col="red"))
 with(data, lines(Date.Time, Sub_metering_3, col="blue"))
+legend(
+    "topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+    col=c("black", "red", "blue"),
+    lty=c("solid", "solid", "solid"))
 
 dev.off()
